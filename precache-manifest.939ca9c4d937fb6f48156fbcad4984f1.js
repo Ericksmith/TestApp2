0284ffb62936627f9c219cad074c5888a0a22959
Erick Smith <smith.s.erick@gmail.com>
2018-10-31 17:17:06 -0700

    Updates

---
+++ precache-manifest.939ca9c4d937fb6f48156fbcad4984f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4ee37abe9c48c5294c6",
+    "url": "/TestApp2/static/js/runtime~main.e4ee37ab.js"
+  },
+  {
+    "revision": "7e6738a196d2609c87cf",
+    "url": "/TestApp2/static/js/main.7e6738a1.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/TestApp2/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "7e6738a196d2609c87cf",
+    "url": "/TestApp2/static/css/main.8b3c7824.chunk.css"
+  },
+  {
+    "revision": "fd5e1fcf34e98c0e9939360bda6509da",
+    "url": "/TestApp2/index.html"
+  }
+];
